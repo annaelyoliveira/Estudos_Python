c70454be72f7ae6b908094caacdfc4fff5b66e4c
annaely <annaely691@gmail.com>
2025-01-11 09:13:24 -0300

    Segundo commit

--- Listas_Exercicios/Desafio84.py
+++ Listas_Exercicios/Desafio84.py
@@ -1,39 +1,5 @@
 dados = []
 pessoas = []
-
-nomesPesados = []
-nomesMenorPeso = []
-while True:
-    dados.append(str(input("Nome:")))
-    dados.append(float(input("Peso:")))
-    pessoas.append(dados[:]) #ATENÇÃO
-    dados.clear()
-   
-    saida = input("Quer continuar? (S/N):")
-    if saida in "Nn":
-        break
-print(pessoas)
-
-print(f"Foram cadastradas {len(pessoas)} pessoas")
-
-menorPeso = pessoas[0][1]
-maiorPeso = pessoas[0][1]
-
-for pessoa in pessoas:
-    if pessoa[1] > maiorPeso:
-        maiorPeso = pessoa[1] 
-        nomesPesados.append(pessoa[0])
-    if pessoa[1] < menorPeso:
-        menorPeso = pessoa[1]
-        nomesMenorPeso.append(pessoa[0])
-
-print(f"O maior peso foi de {maiorPeso}kg. Peso de {nomesPesados}")
-print(f"O menor peso foi de {menorPeso}kg. Peso de {nomesMenorPeso}")
-
-#RESOLUÇÃO PROFESSOR
-
-dados = []
-pessoas = []
 maior = menor = 0 
 
 while True:
@@ -55,12 +21,14 @@ while True:
 print(pessoas)
 
 print(f"Foram cadastradas {len(pessoas)} pessoas")
-print(f"O maior peso foi de {maior}kg. Peso de", end='')
+print(f"O maior peso foi de {maior}kg. Peso de ", end='')
 for pessoa in pessoas: 
     if pessoa[1] == maior:
-        print(f"{pessoa[0]}", end='')
+        print(f"[{pessoa[0]}] ", end='')
 print()
 print(f"O menor peso foi de {maior}kg. Peso de", end='')
 for pessoa in pessoas: 
     if pessoa[1] == menor:
-        print(f"{pessoa[0]}", end='')
+        print(f"[{pessoa[0]}] ", end='')
+
+
